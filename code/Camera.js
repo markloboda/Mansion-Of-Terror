3288193967dd9834a07ab727e597ed98ab4da60c
markloboda <markloboda123@gmail.com>
2021-12-10 14:37:30 +0100

    gltf second commit

--- code/Camera.js
+++ code/Camera.js
@@ -1,3 +1,17 @@
+// import { mat4 } from '../../lib/gl-matrix-module.js';
+
+// export class Camera {
+
+//     constructor(options = {}) {
+//         this.node = options.node || null;
+//         this.matrix = options.matrix
+//             ? mat4.clone(options.matrix)
+//             : mat4.create();
+//     }
+
+// }
+
+
 import { vec3, mat4 } from '../../lib/gl-matrix-module.js';
 
 import { Utils } from './Utils.js';
--- code/Scene.js
+++ code/Scene.js
@@ -1,7 +1,9 @@
+import { Node } from './Node.js';
+
 export class Scene {
 
-    constructor() {
-        this.nodes = [];
+    constructor(options = {}) {
+        this.nodes = [...(options.nodes || [])];
     }
 
     addNode(node) {
@@ -9,7 +11,27 @@ export class Scene {
     }
 
     traverse(before, after) {
-        this.nodes.forEach(node => node.traverse(before, after));
+        for (const node of this.nodes) {
+            this.traverseNode(node, before, after);
+        }
     }
 
+    traverseNode(node, before, after) {
+        if (before) {
+            before(node);
+        }
+        for (const child of node.children) {
+            this.traverseNode(child, before, after);
+        }
+        if (after) {
+            after(node);
+        }
+    }
+
+    clone() {
+        return new Scene({
+            ...this,
+            nodes: this.nodes.map(node => node.clone()),
+        });
+    }
 }
