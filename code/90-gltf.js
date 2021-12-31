ce7632f48c3c1718aed73c1cb8eea4436df6a9fa
zrezke <jeretina.filip@gmail.com>
2021-12-31 11:51:51 +0100

    4 space tabs

--- code/90-gltf.js
+++ code/90-gltf.js
@@ -9,114 +9,115 @@ import { PerspectiveCamera } from "./PerspectiveCamera.js";
 
 
 class App extends Application {
-  async start() {
-    let ext = this.gl.getExtension('OES_texture_float_linear');
-    if (!ext) {
-        console.log("gg")
-    }
-    this.loader = new GLTFLoader();
-    // await this.loader.load('../../common/models/empty_room/empty_room.gltf');
-    // await this.loader.load('../../common/models/cottage/cottage_blender.gltf');
-    // await this.loader.load('../../common/models/room/room.gltf');
-    // await this.loader.load('../../common/models/croc/croc.gltf');
-    // await this.loader.load("../../common/models/example_cube/AnimatedCube.gltf");
-    await this.loader.load("../../common/models/step/InterpolationTest.gltf");
-    // await this.loader.load('https://webglfundamentals.org/webgl/resources/models/killer_whale/whale.CYCLES.gltf');
-    // await this.loader.load('../../common/models/RiggedFigure/RiggedFigure.gltf')
-    this.scene = await this.loader.loadScene(this.loader.defaultScene);
-    console.log(this.scene)
-    // this.camera = await this.loader.loadNode("Camera");
-    const node = new Node({
-      children: [
-        new Node({
-            translation: [0, 2, 3],
-          camera: new PerspectiveCamera({
-            aspect: 1.77,
-            fov: 1,
-            near: 0.1,
-            far: 1000,
-          }),
-        }),
-      ],
-    });
-    this.camera = node.children[0];
-    this.scene.addNode(this.camera);
-    if (!this.scene || !this.camera) {
-      throw new Error("Scene or Camera not present in glTF");
-    }
-
-    if (!this.camera.camera) {
-      throw new Error("Camera node does not contain a camera reference");
-    }
-    this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
-    document.addEventListener(
-      "pointerlockchange",
-      this.pointerlockchangeHandler
-    );
-    this.physics = new Physics(this.scene);
-    this.renderer = new Renderer(this.gl);
-    this.renderer.prepareScene(this.scene);
-    this.resize();
-  }
-
-  enableCamera() {
-    this.canvas.requestPointerLock();
-  }
-
-  render() {
-    if (this.renderer) {
-      this.renderer.render(this.scene, this.camera);
-    }
-  }
-
-  enableCamera() {
-    this.canvas.requestPointerLock();
-  }
-
-  pointerlockchangeHandler() {
-    if (!this.camera) {
-      return;
-    }
-
-    if (document.pointerLockElement === this.canvas) {
-      this.camera.enableMovement();
-    } else {
-      this.camera.disableMovement();
-    }
-  }
-  update() {
-    const t = (this.time = Date.now());
-    const dt = (this.time - this.startTime) * 0.001;
-    this.startTime = this.time;
-    if (this.camera) {
-      this.camera.update(dt);
-    }
-
-    if (this.physics) {
-      this.physics.update(dt);
-    }
-    if (this.scene && this.scene.animations.length) {
-        for (const animation of this.scene.animations) {
-            if (!animation.isActive) {
-                continue;
-            }
-            animation.update();
-        }
-    }
-  }
-  resize() {
-    const w = this.canvas.clientWidth;
-    const h = this.canvas.clientHeight;
-    this.aspect = w / h;
-    if (this.camera) {
-      this.camera.camera.updateCameraMatrix();
-    }
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.querySelector("canvas");
-  const app = new App(canvas);
-  const gui = new GUI();
-  gui.add(app, "enableCamera");
-});
+	async start() {
+		let ext = this.gl.getExtension('OES_texture_float_linear');
+		if (!ext) {
+			console.log("gg")
+		}
+		this.loader = new GLTFLoader();
+		// await this.loader.load('../../common/models/empty_room/empty_room.gltf');
+		// await this.loader.load('../../common/models/cottage/cottage_blender.gltf');
+		// await this.loader.load('../../common/models/room/room.gltf');
+		// await this.loader.load('../../common/models/croc/croc.gltf');
+		// await this.loader.load("../../common/models/example_cube/AnimatedCube.gltf");
+		await this.loader.load("../../common/models/step/InterpolationTest.gltf");
+		// await this.loader.load('https://webglfundamentals.org/webgl/resources/models/killer_whale/whale.CYCLES.gltf');
+		// await this.loader.load('../../common/models/RiggedFigure/RiggedFigure.gltf')
+		this.scene = await this.loader.loadScene(this.loader.defaultScene);
+		console.log(this.scene)
+		// this.camera = await this.loader.loadNode("Camera");
+		const node = new Node({
+			children: [
+				new Node({
+					translation: [0, 2, 3],
+					camera: new PerspectiveCamera({
+						aspect: 1.77,
+						fov: 1,
+						near: 0.1,
+						far: 1000,
+					}),
+				}),
+			],
+		});
+		this.camera = node.children[0];
+		this.scene.addNode(this.camera);
+		if (!this.scene || !this.camera) {
+			throw new Error("Scene or Camera not present in glTF");
+		}
+		
+		if (!this.camera.camera) {
+			throw new Error("Camera node does not contain a camera reference");
+		}
+		this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
+		document.addEventListener(
+			"pointerlockchange",
+			this.pointerlockchangeHandler
+			);
+			this.physics = new Physics(this.scene);
+			this.renderer = new Renderer(this.gl);
+			this.renderer.prepareScene(this.scene);
+			this.resize();
+		}
+		
+		enableCamera() {
+			this.canvas.requestPointerLock();
+		}
+		
+		render() {
+			if (this.renderer) {
+				this.renderer.render(this.scene, this.camera);
+			}
+		}
+		
+		enableCamera() {
+			this.canvas.requestPointerLock();
+		}
+		
+		pointerlockchangeHandler() {
+			if (!this.camera) {
+				return;
+			}
+			
+			if (document.pointerLockElement === this.canvas) {
+				this.camera.enableMovement();
+			} else {
+				this.camera.disableMovement();
+			}
+		}
+		update() {
+			const t = (this.time = Date.now());
+			const dt = (this.time - this.startTime) * 0.001;
+			this.startTime = this.time;
+			if (this.camera) {
+				this.camera.update(dt);
+			}
+			
+			if (this.physics) {
+				this.physics.update(dt);
+			}
+			if (this.scene && this.scene.animations.length) {
+				for (const animation of this.scene.animations) {
+					if (!animation.isActive) {
+						continue;
+					}
+					animation.update();
+				}
+			}
+		}
+		resize() {
+			const w = this.canvas.clientWidth;
+			const h = this.canvas.clientHeight;
+			this.aspect = w / h;
+			if (this.camera) {
+				this.camera.camera.updateCameraMatrix();
+			}
+		}
+	}
+	
+	document.addEventListener("DOMContentLoaded", () => {
+		const canvas = document.querySelector("canvas");
+		const app = new App(canvas);
+		const gui = new GUI();
+		gui.add(app, "enableCamera");
+	});
+	
--- code/animation/Animation.js
+++ code/animation/Animation.js
@@ -1,117 +1,119 @@
 import { quat, vec3 } from "../../lib/gl-matrix-module.js";
 
+
 class AnimationError {
-  static keyframesNotSupplied() {
-    return new Error(
-      "Couldn't instantiate animation: `keyframes` not supplied!"
-    );
-  }
+	static keyframesNotSupplied() {
+		return new Error(
+			"Couldn't instantiate animation: `keyframes` not supplied!"
+		);
+	}
 }
-
-
+	
+	
 /** Animation wrapper class
- *  */
+*  */
 export class Animation {
-  constructor(options = {}) {
-    this.isActive = false;
-    this.name = options.name;
-    this.keyframes = options.keyframes;
-    if (this.keyframes) {
-      this.timestamps = Object.keys(this.keyframes).map((key) => parseInt(key));
-      this.duration = Math.max(...this.timestamps);
-      this.startTime = 0;
-      this.targetKeyframe = 0;
-    } else {
-      throw AnimationError.keyframesNotSupplied();
-    }
-    this.interpolations = {
-      "LINEAR": this.linearInterpolation.bind(this),
-      "STEP": this.stepInterpolation.bind(this),
-      "CUBICSPLINE": this._notImplemented.bind(this)
-    }
-  }
+	constructor(options = {}) {
+		this.isActive = false;
+		this.name = options.name;
+		this.keyframes = options.keyframes;
+		if (this.keyframes) {
+			this.timestamps = Object.keys(this.keyframes).map((key) => parseInt(key));
+			this.duration = Math.max(...this.timestamps);
+			this.startTime = 0;
+			this.targetKeyframe = 0;
+		} else {
+			throw AnimationError.keyframesNotSupplied();
+		}
+		this.interpolations = {
+			"LINEAR": this.linearInterpolation.bind(this),
+			"STEP": this.stepInterpolation.bind(this),
+			"CUBICSPLINE": this._notImplemented.bind(this)
+		}
+	}
+	
+	update() {
+		if (!this.startTime) {
+			this.startTime = Date.now();
+		}
+		
+		let t = Date.now() - this.startTime;
+		if (t > this.timestamps[this.targetKeyframe]) {
+			this.timestamps.length - 1 == this.targetKeyframe
+			? (this.targetKeyframe = 0)
+			: this.targetKeyframe++;
+			if (!this.targetKeyframe) {
+				this.startTime = Date.now();
+				t = 0;
+			}
+		}
+		
+		for (const transform of this.keyframes[this.timestamps[this.targetKeyframe]]) {
+			if (transform.transform.length) {
+				this.interpolations[transform.interpolation](transform, t)
+			}
+		}
+	}
+		
+	linearInterpolation(transform, t) {
+		const keyframeStart = this.targetKeyframe
+		? this.timestamps[this.targetKeyframe - 1]
+		: this.timestamps[this.timestamps.length - 1];
+		const iAmmount = (t - keyframeStart) / (this.timestamps[this.targetKeyframe] - keyframeStart);
+		
+		switch (transform.type) {
+			case "rotation":
+				quat.slerp(
+					transform.node.rotation,
+					transform.node.rotation,
+					transform.transform,
+					iAmmount
+					);
+					break;
+			case "translation":
+				vec3.lerp(
+					transform.node.translation,
+					transform.node.translation,
+					transform.transform,
+					iAmmount);
+				break;
+			case "scale":
+				vec3.lerp(
+					transform.node.scale,
+					transform.node.scale,
+					transform.transform,
+					iAmmount);
+					break;
+			case "weights":
+				this._notImplemented();
+				break;
+		}
+		transform.node.updateMatrix();
+	}
+					
+	stepInterpolation(transform, t) {
+		switch (transform.type) {
+			case "rotation":
+				transform.node.rotation = transform.transform;
+				break;
+			case "translation":
+				transform.node.translation = transform.transform;
+				break;
+			case "scale":
+				transform.node.scale = transform.transform;
+				break;
+			case "weights":
+				this._notImplemented();
+				break;
+		}
+		transform.node.updateMatrix();
+	}
 
-  update() {
-    if (!this.startTime) {
-      this.startTime = Date.now();
-    }
-
-    let t = Date.now() - this.startTime;
-    if (t > this.timestamps[this.targetKeyframe]) {
-      this.timestamps.length - 1 == this.targetKeyframe
-        ? (this.targetKeyframe = 0)
-        : this.targetKeyframe++;
-      if (!this.targetKeyframe) {
-        this.startTime = Date.now();
-        t = 0;
-      }
-    }
-
-    for (const transform of this.keyframes[this.timestamps[this.targetKeyframe]]) {
-      if (transform.transform.length) {
-        this.interpolations[transform.interpolation](transform, t)
-      }
-    }
-  }
-
-  linearInterpolation(transform, t) {
-    const keyframeStart = this.targetKeyframe
-      ? this.timestamps[this.targetKeyframe - 1]
-      : this.timestamps[this.timestamps.length - 1];
-    const iAmmount = (t - keyframeStart) / (this.timestamps[this.targetKeyframe] - keyframeStart);
-
-    switch (transform.type) {
-      case "rotation":
-        quat.slerp(
-          transform.node.rotation,
-          transform.node.rotation,
-          transform.transform,
-          iAmmount
-        );
-        break;
-      case "translation":
-        vec3.lerp(
-          transform.node.translation,
-          transform.node.translation,
-          transform.transform,
-          iAmmount);
-        break;
-      case "scale":
-        vec3.lerp(
-          transform.node.scale,
-          transform.node.scale,
-          transform.transform,
-          iAmmount);
-        break;
-      case "weights":
-        this._notImplemented();
-        break;
-    }
-    transform.node.updateMatrix();
-  }
-  
-  stepInterpolation(transform, t) {
-    switch (transform.type) {
-      case "rotation":
-        transform.node.rotation = transform.transform;
-        break;
-      case "translation":
-          transform.node.translation = transform.transform;
-        break;
-      case "scale":
-          transform.node.scale = transform.transform;
-        break;
-      case "weights":
-        this._notImplemented();
-        break;
-    }
-    transform.node.updateMatrix();
-  }
-
-  _notImplemented(...args) {
-    if (!this.logged) {
-      console.error("Your type of animation is not supported!");
-      this.logged = true
-    }
-  }
+	_notImplemented(...args) {
+		if (!this.logged) {
+			console.error("Your type of animation is not supported!");
+			this.logged = true
+		}
+	}
 }
+				
